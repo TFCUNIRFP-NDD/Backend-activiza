d2c3bab6fd9b6323a371d8fc4ae3b32f9d780fd3
Daniel Barragues <25356150+Barraguesh@users.noreply.github.com>
2024-04-09 22:29:02 +0200

    Cambios despliegue

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -4,4 +4,4 @@ set -e
 
 python manage.py collectstatic --no-input
 
-uwsgi --socket :8000 --master --enable-threads --module dashpack.wsgi
+uwsgi --socket :8000 --master --enable-threads --module backend.wsgi
