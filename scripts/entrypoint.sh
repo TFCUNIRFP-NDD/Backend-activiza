a7c45fc9a9ea9276982e686b71a5d4a4227817f5
Daniel Barragues <25356150+Barraguesh@users.noreply.github.com>
2024-04-17 14:44:36 +0200

    Auto migrate fix

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -4,6 +4,4 @@ set -e
 
 #Auto migrate para aplicar cambios en la BDD
 python manage.py migrate
-python manage.py collectstatic --no-input
-
-uwsgi --socket :80 --master --enable-threads --module backend.wsgi
+python manage.py runserver 0.0.0.0:80
