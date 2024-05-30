0830ae1b0af4de86510cdd0b6a47360651b858f7
Daniel Barragues <25356150+Barraguesh@users.noreply.github.com>
2024-05-30 20:47:51 +0200

    Cambios de tokens no en admin

--- backend/activiza/apps.py
+++ backend/activiza/apps.py
@@ -1,6 +1,15 @@
 from django.apps import AppConfig
 
-
 class ActivizaConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'activiza'
+    verbose_name = "Activiza"
+
+    def ready(self):
+        # This method will be executed once Django is fully initialized
+        from django.contrib import admin
+        from rest_framework.authtoken.models import TokenProxy #or Token based on your Django version
+        try:
+            admin.site.unregister(TokenProxy)
+        except admin.sites.NotRegistered:
+            pass
